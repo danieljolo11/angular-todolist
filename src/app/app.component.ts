c9d5e9ef201cae006e8522de086e51d57a1eec86
Daniel Jolo <danieljolo4899@gmail.com>
2023-03-14 10:51:21 +0800

    crud using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
 
+interface Form {
+  username: string;
+  index?: number;
+}
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-todolist';
+  public title = 'angular-todolist';
+  public message: string = 'Hello World';
+  public Data: Array<Form> = [];
+  public isButtonVisible = false;
+
+  public user: Form = {
+    username: '',
+  };
+
+  public edituser: Form = {
+    username: '',
+    index: 0,
+  };
+
+  public adduser = (value: any) => {
+    this.Data.push(this.user);
+    this.user = { username: '' };
+  };
+
+  public updateuser = (value: any) => {
+    const { index, username }: Form | any = this.edituser || {};
+
+    this.Data[index].username = username;
+    this.isButtonVisible = false;
+    return (this.edituser = { username: '' });
+  };
+
+  public onEdit = (data: string, index: number) => {
+    this.edituser = { username: data, index: index };
+    this.isButtonVisible = true;
+  };
+
+  public onRemove = (userindex: number) => {
+    this.Data.forEach((value, index) => {
+      if (index === userindex) this.Data.splice(index, 1);
+    });
+  };
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  separator: "_",
+  content: ["./src/**/*.{html,ts,pug}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
